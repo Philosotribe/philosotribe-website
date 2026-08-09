4f25a8482176c78e5f377876e06c6abecfdbf1fc
Philosotribe <philosotribe@gmail.com>
2026-08-08 21:02:39 -0600

    Update site-version.js

--- assets/js/site-version.js
+++ assets/js/site-version.js
@@ -2,4 +2,4 @@
 // Increment this value every time you publish an updated website.
 // The HTML bootstrap always requests this tiny file with a unique URL, so browsers
 // can discover the new version even when an older page is still cached.
-window.PHILOSOTRIBE_SITE_VERSION = "2026-08-08-1";
+window.PHILOSOTRIBE_SITE_VERSION = "2026-08-08-2";
